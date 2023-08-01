9b62adc697afadbc89114087342f896511304e92
Ripty-Dx <riptyfullstack.dx@gmail.com>
2023-08-01 18:29:59 +0530

    login ui

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,51 @@
 import React from "react";
 import "./Login.css";
+import image from "../images/BFS-login-page1.jpg";
+import logo from "../images/BeautyFashionLogo.png";
 const Login = () => {
   return (
     <>
-      <div>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-6 col-lg-6 p-0 m-0 d-none d-md-block image-section">
+            <img src={image} height={"100%"}  width={"100%"} alt="makeup" />
+          </div>
+          <div className="col-md-6 col-lg-6 col-sm-12 p-5 pb-0 ">
+            <div className="d-flex justify-content-center mt-3">
+              <img src={logo} alt="logo" height={"130%"} width={"70%"}/>
+            </div>
+            <div className="d-flex justify-content-center mt-3">
+              <p className="logo-text" >BFS PORTAL</p>
+            </div>
+            <div className="mt-2">
+              <h3 className="c-gray detail-text">Enter details to login below</h3>
+            </div >
+            <div className="mt-3 p-2">
+              <form>
+                <div className="row">
+                    <input type="text" className="loginInput" name="userName" placeholder="UserName"/>
+                </div>
+                <div className="row mt-4">
+                <input type="password" className="loginInput" name="password"  placeholder="Password"/>
 
+                </div>
+                <div className="row">
+                   <div className="col-md-6  d-flex justify-content-center mt-4 ">
+                    <a href="#1213827" className="text-dark forgotPassword">Forgot Password?</a>
+                   </div>
+                   <div className="col-md-6 d-flex justify-content-center mt-4 ">
+                    <button className="bg-gray text-white login-button"> Login</button>
+                   </div>
+                </div>
+                <hr></hr>
+                <div className="d-flex justify-content-center mt-5 apply mb-0">
+                Don't have an account?
+                <a href="#1213827" className="text-dark fw-bold">Apply now</a>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
