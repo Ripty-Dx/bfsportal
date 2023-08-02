e841f239d6100fd27581c725d380eec3856c08b3
Ripty-Dx <riptyfullstack.dx@gmail.com>
2023-08-02 16:09:33 +0530

    Login UI for tablet mode

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,11 +7,12 @@ const Login = () => {
   return (
     <>
       <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-6 col-lg-6 p-0 m-0 d-none d-md-block image-section">
-            <img src={image} height={"100%"}  width={"100%"} alt="makeup" />
+        <div className="row bg-image">
+        <div className="col-md-3 col-lg-6 p-0 m-0  image-section ">
+            {/* <img src={image} height={"100%"}  width={"100%"} alt="makeup" /> */}
           </div>
-          <div className="col-md-6 col-lg-6 col-sm-12 p-5 pb-0 ">
+          <div className="col-md-9 col-lg-6 col-sm-12 form-part">
+            <div className="display-part">
             <div className="d-flex justify-content-center mt-3">
               <img src={logo} alt="logo" height={"120%"} width={"70%"}/>
             </div>
@@ -22,16 +23,16 @@ const Login = () => {
               <h3 className="c-gray detail-text">Enter detail to login below</h3>
             </div >
             <div className="mt-3 p-2">
-              <form>
-                <div className="row">
+              <form id="form-input">
+                <div className="row form-input" >
                     <input type="text" className="loginInput" name="username" placeholder="Username"/>
                 </div>
-                <div className="row mt-4">
+                <div className="row mt-4 form-input">
                 <input type="password" className="loginInput" name="password"  placeholder="Password"/>
 
                 </div>
                 <div className="row">
-                   <div className="col-md-6  d-flex justify-content-center mt-4 p-1">
+                   <div className="col-md-6  d-flex justify-content-center align-items-center mt-4 p-1">
                     <a href="#1213827" className="text-dark forgotPassword">Forgot Password?</a>
                    </div>
                    <div className="col-md-6 d-flex justify-content-center mt-4 p-1 ">
@@ -40,11 +41,12 @@ const Login = () => {
                 </div>
                 <hr></hr>
                 <div className="d-flex justify-content-center mt-5 apply mb-0">
-                Don't have an account?
+                Don't have an account? &nbsp;
                 <a href="#1213827" className="text-dark fw-bold">Apply now</a>
                 </div>
               </form>
             </div>
+          </div>
           </div>
         </div>
       </div>
