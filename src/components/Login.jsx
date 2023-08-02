b66ba53501c5d262385996f14dc69ec2604a1401
Ripty-Dx <riptyfullstack.dx@gmail.com>
2023-08-02 11:33:33 +0530

    corrected login ui

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,6 +3,7 @@ import "./Login.css";
 import image from "../images/BFS-login-page1.jpg";
 import logo from "../images/BeautyFashionLogo.png";
 const Login = () => {
+
   return (
     <>
       <div className="container-fluid">
@@ -12,28 +13,28 @@ const Login = () => {
           </div>
           <div className="col-md-6 col-lg-6 col-sm-12 p-5 pb-0 ">
             <div className="d-flex justify-content-center mt-3">
-              <img src={logo} alt="logo" height={"130%"} width={"70%"}/>
+              <img src={logo} alt="logo" height={"120%"} width={"70%"}/>
             </div>
             <div className="d-flex justify-content-center mt-3">
-              <p className="logo-text" >BFS PORTAL</p>
+              <p className="logo-text"  >BFS PORTAL</p>
             </div>
             <div className="mt-2">
-              <h3 className="c-gray detail-text">Enter details to login below</h3>
+              <h3 className="c-gray detail-text">Enter detail to login below</h3>
             </div >
             <div className="mt-3 p-2">
               <form>
                 <div className="row">
-                    <input type="text" className="loginInput" name="userName" placeholder="UserName"/>
+                    <input type="text" className="loginInput" name="username" placeholder="Username"/>
                 </div>
                 <div className="row mt-4">
                 <input type="password" className="loginInput" name="password"  placeholder="Password"/>
 
                 </div>
                 <div className="row">
-                   <div className="col-md-6  d-flex justify-content-center mt-4 ">
+                   <div className="col-md-6  d-flex justify-content-center mt-4 p-1">
                     <a href="#1213827" className="text-dark forgotPassword">Forgot Password?</a>
                    </div>
-                   <div className="col-md-6 d-flex justify-content-center mt-4 ">
+                   <div className="col-md-6 d-flex justify-content-center mt-4 p-1 ">
                     <button className="bg-gray text-white login-button"> Login</button>
                    </div>
                 </div>
