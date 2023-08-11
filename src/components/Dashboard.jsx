1ab95ca03db1b1d342d5973e214be1f2adbad1f8
Ripty-Dx <riptyfullstack.dx@gmail.com>
2023-08-11 18:32:49 +0530

    capsule module

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,6 +3,7 @@ import Header1 from "./Header1";
 import Footer from "./Footer";
 import "./Dashboard.css";
 import { LiaSortAlphaDownSolid } from "react-icons/lia";
+import { BsFillPersonFill } from "react-icons/bs";
 const Dashboard = () => {
   return (
     <>
@@ -11,31 +12,30 @@ const Dashboard = () => {
           <div className="">
             <Header1 />
             <div className="container-fluid">
-              <div className="row d-flex align-items-center justify-content-center ">
+              <div className="row d-flex align-items-center justify-content-md-center">
                 {/* Your account heading */}
-                <div className="col-lg-5 m-1 p-lg-2 col-md-4 p-md-1 ps-md-3">
+                <div className="col-lg-5 m-md-1 p-lg-2 col-md-auto py-md-1 ps-md-3 mx-md-auto m-sm-2   ">
                   <h2 className="fw-bold text-decoration-underline fw-md-normal link-offset-2">
                     Your Accounts
                   </h2>
                 </div>
                 {/* sort button */}
-
-                {/* <div className="col-lg-8 m-1 p-2"> */}
-                <div className=" col-lg-auto  col-md-2 d-flex p-lg-2 d-flex align-items-center justify-content-center">
+                <div className=" col-lg-auto p-md-0 col-md-2 d-flex p-lg-2 d-flex align-items-center justify-content-lg-center justify-content-sm-left">
                   Sort By: &nbsp;
                   <button className="btn btn-light fs-4 px-2 shadow-sm bg-white  pt-0 m-0">
                     <LiaSortAlphaDownSolid />
                   </button>
                 </div>
                 {/* dropdown button */}
-                <div className=" col-lg-auto col-md-auto p-md-0  p-lg-2 dropdown">
+                <div className=" col-lg-auto col-md-auto p-md-0  p-lg-2   dropdown">
                   {/* <div className=""> */}
                   <button
                     type="button"
                     className="btn btn-light dropdown-toggle shadow-sm bg-white"
                     data-bs-toggle="dropdown"
                   >
-                    Manufactured By          </button>
+                    Manufactured By{" "}
+                  </button>
                   <ul class="dropdown-menu">
                     <li>
                       <a class="dropdown-item" href="#1">
@@ -56,7 +56,7 @@ const Dashboard = () => {
                   {/* </div> */}
                 </div>
                 {/* search button */}
-                <div className=" col-lg-2 col-md-2  p-lg-2">
+                <div className=" col-lg-2 col-md-2  p-lg-2 m-xs-5">
                   <form>
                     <input
                       type="search"
@@ -66,15 +66,56 @@ const Dashboard = () => {
                   </form>
                 </div>
                 {/* reset button */}
-                <div className=" col-lg-1 p-md-0 col-md-1">
+                <div className=" col-lg-1 p-md-0 col-md-1 ">
                   {/* <button className="btn " id="resetButton"> */}
-                  <button className="btn btn-outline-secondary ">Reset</button>
+                  <button className="btn btn-outline-secondary">Reset</button>
                 </div>
-                {/* </div> */}
+              </div>
+
+              <div className="row d-flex gap-1 justify-content-center mt-3">
+                <div className="col-md-3 ">
+                  <div className="bg-white shadow rounded-3 d-flex align-items-center justify-content-center">
+                    <span className="px-4  m-1 p-2">
+                      <BsFillPersonFill
+                        size={"35px"}
+                        className="p-1 text-white rounded-circle border-0 icon"
+                      />
+                    </span>
+                    <div className="transition mx-auto">
+                      <button className="btn fw-bold ">Apothe</button>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-3 ">
+                  <div className="bg-white shadow rounded-3 d-flex align-items-center justify-content-center">
+                    <span className="px-4  m-1 p-2">
+                      <BsFillPersonFill
+                        size={"35px"}
+                        className="p-1 text-white rounded-circle border-0 icon"
+                      />
+                    </span>
+                    <div className="transition mx-auto">
+                      <button className="btn fw-bold ">Apothe</button>
+                    </div>
+                  </div>
+                </div><div className="col-md-3 ">
+                  <div className="bg-white shadow rounded-3 d-flex align-items-center justify-content-center">
+                    <span className="px-4  m-1 p-2">
+                      <BsFillPersonFill
+                        size={"35px"}
+                        className="p-1 text-white rounded-circle border-0 icon"
+                      />
+                    </span>
+                    <div className="transition mx-auto">
+                      <button className="btn fw-bold ">Apothe</button>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
             {/* Footer */}
             <div className="fixed-bottom">
+              {/* <div className=""> */}
               <Footer />
             </div>
           </div>
